5d0e6765b50f7dec148c70e49b38b49f8e1d711c
Michael King <mking@hey.com>
2020-12-12 20:29:12 -0500

    finished day 1

--- src/d1.rs
+++ src/d1.rs
@@ -2,21 +2,55 @@
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
 
-pub fn run() {
-    println!("Hello from d1!");
+fn d1p1(filename: impl AsRef<Path>) {
+    println!("Day 1, Part 1");
 
-    let file_name = "/Users/michael/Projects/advent-of-code/src/input.txt";
-
-    let file = File::open(&file_name).expect("file not found");
+    let file = File::open(filename).expect("file not found");
     let reader = BufReader::new(file);
-    // let contents = fs::read_to_string("input.txt")
-    //     .expect("Something went wrong reading the file");
+    let lines: Vec<u32> = reader.lines()
+                    .map(|l| l.unwrap().parse::<u32>().expect("could not read line"))
+                    .collect();
 
-    for line in reader.lines() {
-        // func(&line.unwrap());
-        println!("{}", &line.unwrap());
+    for num_1 in &lines {
+        for num_2 in &lines {
+
+            if num_1 + num_2 == 2020 {
+                println!("{} + {} == 2020", num_1, num_2);
+                return println!("{} * {} == {}", num_1, num_2, num_1 * num_2);
+            }
+        }
     }
+}
 
-    // println!("input.txt:\n{}", contents);
+fn d1p2(filename: impl AsRef<Path>) {
+    println!("Day 1, Part 2");
+
+    let file = File::open(filename).expect("file not found");
+    let reader = BufReader::new(file);
+    let lines: Vec<u32> = reader.lines()
+                    .map(|l| l.unwrap().parse::<u32>().expect("could not read line"))
+                    .collect();
+
+    for num_1 in &lines {
+        for num_2 in &lines {
+            for num_3 in &lines {
+
+                if num_1 + num_2 + num_3 == 2020 {
+                    println!("{} + {} + {} == 2020", num_1, num_2, num_3);
+                    return println!("{} * {} * {} == {}",
+                        num_1, num_2, num_3,
+                        num_1 * num_2 * num_3
+                    );
+                }
+            }
+        }
+    }
+}
+
+pub fn run(filename: impl AsRef<Path>) {
+    d1p1(&filename);
+    println!();
+    d1p2(&filename);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod d1;
 
 fn main() {
-    println!("Hello, Advent of Code!");
-    d1::run();
+    println!("Hello, Advent of Code!\n");
+    d1::run("./src/input.txt");
 }
