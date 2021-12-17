2dc58249eecc0aed6688f99f162c80fed14b3667
Michael King <m.king@fastmail.com>
2021-12-17 15:19:35 -0500

    day 3 part 2

--- 2021/src/day3.rs
+++ 2021/src/day3.rs
@@ -1,8 +1,7 @@
 use crate::Result;
 
-pub fn part1<T: Iterator<Item = String>>(data: T) -> Result<i32> {
-    let report: Vec<String> = data.collect();
-    let mut gamma_counts = vec![0; report[0].chars().count()];
+pub fn get_gamma_epsilon_rates(report: &Vec<String>) -> Result<(String, String)> {
+    let mut gamma_counts = vec![0; *&report[0].chars().count()];
 
     for bin_str in report {
         bin_str.chars().enumerate().for_each(|(i, c)| {
@@ -13,11 +12,12 @@ pub fn part1<T: Iterator<Item = String>>(data: T) -> Result<i32> {
             }
         });
     }
+    // println!("gamma_counts: {:?}", gamma_counts);
 
     let mut gamma_rate = String::new();
     let mut epsilon_rate = String::new();
     for count in gamma_counts {
-        if count > 0 {
+        if count >= 0 {
             gamma_rate.push('1');
             epsilon_rate.push('0');
         } else {
@@ -25,6 +25,12 @@ pub fn part1<T: Iterator<Item = String>>(data: T) -> Result<i32> {
             epsilon_rate.push('1');
         }
     }
+    Ok((gamma_rate, epsilon_rate))
+}
+
+pub fn part1<T: Iterator<Item = String>>(data: T) -> Result<i32> {
+    let report: Vec<String> = data.collect();
+    let (gamma_rate, epsilon_rate) = get_gamma_epsilon_rates(&report)?;
 
     let gamma_val = i32::from_str_radix(&gamma_rate, 2).unwrap();
     let epsilon_val = i32::from_str_radix(&epsilon_rate, 2).unwrap();
@@ -32,6 +38,34 @@ pub fn part1<T: Iterator<Item = String>>(data: T) -> Result<i32> {
     // println!("{} {}", gamma_val, epsilon_val);
 
     Ok(gamma_val * epsilon_val)
+}
+
+pub fn part2<T: Iterator<Item = String>>(data: T) -> Result<i32> {
+    let mut report: Vec<String> = data.collect();
+    let mut report_copy = report.clone();
+    println!("Full report: {:?}", report);
+
+    let mut gamma_rate = get_gamma_epsilon_rates(&report)?.0;
+    let mut epsilon_rate = get_gamma_epsilon_rates(&report_copy)?.1;
+
+    let mut i = 0;
+    while report.len() > 1 || report_copy.len() > 1 {
+        if report.len() > 1 {
+            report.retain(|d| d.chars().nth(i).unwrap() == gamma_rate.chars().nth(i).unwrap());
+        }
+        if report_copy.len() > 1 {
+            report_copy.retain(|d| d.chars().nth(i).unwrap() == epsilon_rate.chars().nth(i).unwrap());
+        }
+        gamma_rate = get_gamma_epsilon_rates(&report)?.0;
+        epsilon_rate = get_gamma_epsilon_rates(&report_copy)?.1;
+        i += 1;
+        // println!("{:?}", report_copy);
+    }
+
+    let oxygen_generator_rating = i32::from_str_radix(&report[0], 2).unwrap();
+    let co2_scrubber_rating = i32::from_str_radix(&report_copy[0], 2).unwrap();
+
+    Ok(oxygen_generator_rating * co2_scrubber_rating)
 }
 
 #[cfg(test)]
@@ -57,21 +91,21 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn test_example_2() -> Result<()> {
-    //     assert_eq!(
-    //         part2(util::example(2, 1).flat_map(|line| line.parse()))?,
-    //         900
-    //     );
-    //     Ok(())
-    // }
+    #[test]
+    fn test_example_2() -> Result<()> {
+        assert_eq!(
+            part2(util::example(3, 1).flat_map(|line| line.parse()))?,
+            230
+        );
+        Ok(())
+    }
 
-    // #[test]
-    // fn part2_test() -> Result<()> {
-    //     assert_eq!(
-    //         Some(part2(util::input(2).flat_map(|line| line.parse()))?),
-    //         util::answer(2, 2)
-    //     );
-    //     Ok(())
-    // }
+    #[test]
+    fn part2_test() -> Result<()> {
+        assert_eq!(
+            Some(part2(util::input(3).flat_map(|line| line.parse()))?),
+            util::answer(3, 2)
+        );
+        Ok(())
+    }
 }
